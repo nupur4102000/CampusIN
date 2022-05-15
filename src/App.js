3e9ee0159449c81eb0cad0b2baa11fb1a09c556b
nupur4102000 <91957347+nupur4102000@users.noreply.github.com>
2022-05-16 01:39:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-//import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-//import Header from "./components/Header";
-import Home from "./components/Home";
-//import Login from "./components/Login";
-
+import Tmp from "./components/Tmp"
+import Header from "./components/Header"
+//import Login from "./components/Login"
 function App() {
   return (
     <div className="App">
-      {
-           <Home/>
-      /* <Router>
-        <Switch>
-          <Route exact path="/">
-            <Login />
-          </Route>
-          <Route path="/home">
-            <Header />
-            <Home />
-          </Route>
-        </Switch>
-      </Router> */}
+      
+           
+       <Router>
+        <Routes>
+          <Route path="/" element={<Tmp/>}/>
+          <Route path="/home" element={<Header/> }/>
+            
+          
+          
+            
+          
+        </Routes>
+      </Router> 
     </div>
   );
 }
